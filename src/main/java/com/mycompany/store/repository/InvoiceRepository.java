9ce502cc7b9473fe48299eeebf7d8e90e822f1b0
Zuyevich Aliaksandr <azuyevich@exadel.net>
2021-05-11 21:41:33 +0300

    update role based authorization logic

--- src/main/java/com/mycompany/store/repository/InvoiceRepository.java
+++ src/main/java/com/mycompany/store/repository/InvoiceRepository.java
@@ -1,7 +1,11 @@
 package com.mycompany.store.repository;
 import com.mycompany.store.domain.Invoice;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 
 /**
@@ -10,5 +14,6 @@ import org.springframework.stereotype.Repository;
 @SuppressWarnings("unused")
 @Repository
 public interface InvoiceRepository extends JpaRepository<Invoice, Long> {
-
+    Page<Invoice> findAllByOrderCustomerUserLogin(String login, Pageable pageable);
+    Optional<Invoice> findOneByIdAndOrderCustomerUserLogin(Long id, String login);
 }
